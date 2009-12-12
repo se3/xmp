ea40790295efb4e0a2956d0f93cacd8556ff5ec1
funpc3 <funpc3@8d3e5308-dac3-11de-8a1a-419b527e5386>
2009-12-12 21:58:44 +0000

    xmp: reworked base installation.  - added ipkg-cl binary  - removed static opt archive ( with unused apps for base install )

--- xmp/programs/base/install_base.sh
+++ xmp/programs/base/install_base.sh
@@ -1,25 +1,42 @@
 #!/bin/sh
 
-cd /
-tar -xvf /tmp/usbmounts/sda1/xmp/programs/base/install.tar
-/opt/bin/ipkg update
+echo "installing uclibc-opt "
+if [ ! -f uclibc-opt_0.9.28-13_mipsel.ipk ]; then wget http://ipkg.nslu2-linux.org/feeds/optware/oleg/cross/stable/uclibc-opt_0.9.28-13_mipsel.ipk; fi
+./ipkg-cl install uclibc-opt_0.9.28-13_mipsel.ipk 
 
-echo "Copy preconfig."
+echo "installing ipkg-opt"
+if [ ! -f ipkg-opt_0.99.163-10_mipsel.ipk ]; then wget http://ipkg.nslu2-linux.org/feeds/optware/oleg/cross/stable/ipkg-opt_0.99.163-10_mipsel.ipk; fi
+./ipkg-cl install ipkg-opt_0.99.163-10_mipsel.ipk
 
-cp -R /tmp/usbmounts/sda1/xmp/programs/base/preconf/* /
-/bin/chmod 644 /etc/init.d/S45telnet
-/bin/chmod 644 /etc/init.d/S55mount
-echo "Copy done."
-echo ""
+# check if /opt/bin/ipkg installed properly
+if [ -f /opt/bin/ipkg ]; then
 
-echo ""
-echo "ALL INSTALL DONE"
-echo ""
-echo "Installed programs:"
-echo "- IPKG - package managger (updated package list)"
-echo "- ntp"
-echo "- cron"
-echo "- unrar"
-echo "- busybox"
-echo ""
-echo "- preconfig files"
+   echo "update opt package"
+   /opt/bin/ipkg update
+   
+   echo "installing cron for timer jobs"
+   /opt/bin/ipkg install cron
+   
+   echo "installing ntp for time synchronization"
+   /opt/bin/ipkg install ntp
+   
+   echo "installing unrar for unpack rar files"
+   /opt/bin/ipkg install unrar
+   
+   echo "Copy preconfig."
+   cp -R preconf/* /
+   
+   /bin/chmod 644 /etc/init.d/S45telnet
+   /bin/chmod 644 /etc/init.d/S55mount
+   
+   echo ""
+   echo "ALL INSTALL DONE"
+   echo "INSTALLED OPTWARE PROGRAMS:"
+   /opt/bin/ipkg list_installed
+   echo ""
+   echo "- preconfig files"
+   
+else
+
+   echo "INSTALL FAILED - missing internet connection?"
+fi
