7139190fd007c4fd6fb834aeb125acdb8a78feda
arpad@santha.net <arpad@santha.net@8d3e5308-dac3-11de-8a1a-419b527e5386>
2009-12-20 09:42:40 +0000

    new 2.1.2 compatibile original skin added new skin added (maked by frodo) old skin instaler phps removed

---
+++ xmp/skins/frodo/skininstall.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1250" />
+<title>Xtreamer Mod Pack - Skin</title>
+<link rel="stylesheet" type="text/css" href="../../xmp.css">
+<body>
+<META HTTP-EQUIV=Refresh CONTENT="30; URL=../../info.php">
+<?
+echo '<pre>';
+system('unzip -o *.zip -d /usr/local/bin/Resource/bmp/', $retval);
+echo '</pre>';
+if ( $retval == "0") echo 'Install done.'; else echo 'Install failed!';
+
+?>
+</body>
+</html>
