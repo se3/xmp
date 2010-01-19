9a42fd84f579e527c53da1f0e914d31e34ac5fed
funpc3 <funpc3@8d3e5308-dac3-11de-8a1a-419b527e5386>
2010-01-19 22:37:06 +0000

    xmp: added transmission-queue. You have to enable cron app.

--- xmp/programs/transmission/install_transmission.sh
+++ xmp/programs/transmission/install_transmission.sh
@@ -5,4 +5,6 @@ echo "Transmission install started."
 echo "Transmission install done."
 echo ""
 cp -R /tmp/usbmounts/sda1/xmp/programs/transmission/preconf/* /
+echo "*/5 * * * * root /root/transmission/transmission-queue" >> /opt/var/cron/crontabs/root
 /bin/chmod 644 /etc/init.d/S227transmission
+/bin/chmod 755 /root/transmission/transmission-queue
