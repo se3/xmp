46f210e7512cab60cdc5e6e6c9e832f1de1db3af
funpc3 <funpc3@8d3e5308-dac3-11de-8a1a-419b527e5386>
2010-01-20 19:50:42 +0000

    xmp: corrected the transmission process priority. from 9 -> to 19

--- xmp/programs/transmission/preconf/bin/transmission_start.sh
+++ xmp/programs/transmission/preconf/bin/transmission_start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 PATH=/opt/bin:/opt/bin:$PATH
-nice -n 9 /opt/bin/transmission-daemon -g /root/transmission/
+nice -n 19 /opt/bin/transmission-daemon -g /root/transmission/
 echo "Transmisssion-daemon started"
 sleep 2
 ps | grep transmission-daemon
